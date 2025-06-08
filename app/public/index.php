77976cf17f7c0eb17729085d749e9cbf4d8ee333
scottbrazil <scott7rj@gmail.com>
2025-06-08 11:03:57 -0300

    product added

--- app/public/index.php
+++ app/public/index.php
@@ -1,6 +1,7 @@
 <?php
 use App\Model\User;
-require_once dirname(__DIR__) . '/vendor/autoload.php';
+use App\Model\Product;
+require dirname(__DIR__) . '/vendor/autoload.php';
 
 echo "<h1>Hello World!!!</h1>";
 echo $_SERVER['SCRIPT_FILENAME'] . "<br />";
@@ -10,9 +11,12 @@ $arg1 = escapeshellarg('hello');
 $arg2 = escapeshellarg('world');
 //$output = shell_exec("nohup python3 /opt/test.py $arg1 $arg2 > /dev/null 2>&1 &");
 $output = shell_exec("python3 /opt/test.py $arg1 $arg2");
-echo $output;
+echo $output . "<br />";
 
 $user = new User('John Doe');
-echo $user->getName();
+echo $user->getName() . "<br />";
+
+$product = new Product('Motorola');
+echo $product->getName() . "<br />";
 
 phpinfo();
